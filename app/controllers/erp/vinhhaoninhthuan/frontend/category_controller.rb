9821aa464f4fa8a8b550c416de40e8a427dc5ab6
sonnguyenngoc <sonnn@hoangkhang.com.vn>
2017-04-16 08:08:29 -0700

    edit page

---
+++ app/controllers/erp/vinhhaoninhthuan/frontend/category_controller.rb
@@ -0,0 +1,13 @@
+module Erp
+  module Vinhhaoninhthuan
+    module Frontend
+      class CategoryController < Erp::Frontend::FrontendController
+        def listing
+        end
+        
+        def product_detail
+        end
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Erp::Vinhhaoninhthuan::Engine.routes.draw do
   root to: "frontend/home#index"
+  get "chuyen-muc-san-pham.html" => "frontend/category#listing", as: :product_listing
+  get "chuyen-muc-san-pham/chi-tiet-san-pham.html" => "frontend/category#product_detail", as: :product_detail
 end
